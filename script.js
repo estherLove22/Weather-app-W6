f2c6e5f2bb9398bd92c4cb978cccdc594217c66c
estherLove22 <91344188+estherLove22@users.noreply.github.com>
2021-11-28 17:01:16 -0500

    update time

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
-function formatDate() {
-    let hour = now.getHours();
-    if (hour < 10) {
-      hour = `0${hour}`;
-    }
+function formatDate(timestamp){
+let  date = new Date (timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day = days[date.getDay()];
+
+
+return `${day} ${hours}: ${minutes}`;
+
+}
+
   
-    let minute = now.getMinutes();
-    if (minute < 10) {
-      minute = `0${minute}`;
-    }
-  
-    let weeks = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-  
-    let month = months[now.getMonth()];
-    let week = weeks[now.getDay()];
-    let date = now.getDate();
-  
-    return `${week} ${hour}:${minute} ${month} ${date}`;
-  }
-  
-  let currentTime = document.querySelector("#first-day");
-  let now = new Date();
-  
-  currentTime.innerHTML = formatDate(now);
-  
- function showTemperature(response){
+function showTemperature(response){
    console.log(response.data);
 
 let temperature = document.querySelector("h3");
@@ -55,21 +38,25 @@ let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#wind");
 let high = document.querySelector("#high");
 let low = document.querySelector("#low");
+let date = document.querySelector("#date");
+
 
 
 temperature.innerHTML = `${Math.round(response.data.main.temp)}° `;
 city.innerHTML = response.data.name;
 description.innerHTML = response.data.weather[0].description;
-humidity.innerHTML = `Humiidty:${response.data.main.humidity}%`;
+humidity.innerHTML = `Humidity:${response.data.main.humidity}%`;
 windSpeed.innerHTML =  `Wind:${Math.round(response.data.wind.speed)} Km/Hr`;
 high.innerHTML = `High:${Math.round(response.data.main.temp_max)}° `;
 low.innerHTML = `Low:${Math.round(response.data.main.temp_min)}° `;
+date.innerHTML =formatDate(response.data.dt * 1000);
 
  } 
 
  let apiKey = "c48f152f0e04033d01dba3c6ec2d4606";
  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
  let units = `metric`;
- let apiUrl = `${apiEndpoint}?q=Toronto&appid=${apiKey}&units=${units}`;
+ let city = "Toronto";
+ let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
 
  axios.get(apiUrl).then(showTemperature);
